2ccaba8bde414665babb49c78cf92aaaef57bc18
Wittawas Nakkasem <vittee@hotmail.com>
2021-02-13 01:02:50 +0700

    Begin actual app development.

---
+++ packages/app/webpack.main.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const factory = (env, args) => {
+  return {
+    target: 'electron-main',
+    mode: 'development',
+    entry: './src/main/index.ts',
+    output: {
+      path: path.resolve(__dirname, 'bundle'),
+      pathinfo: true,
+      filename: 'main.js'
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.jsx?$/, /\.tsx?$/],
+          use: 'babel-loader'
+        }
+      ]
+    }
+  }
+}
+
+module.exports = factory;
