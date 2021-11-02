8a38c46253d982c49a9a5bae78172d84a8c953f2
Wittawas Nakkasem <vittee@hotmail.com>
2021-11-03 03:31:45 +0700

    Improve track queue and metadata reading

--- packages/core/src/boombox.ts
+++ packages/core/src/boombox.ts
@@ -1,20 +1,28 @@
 import _, { flatten, some, uniq } from "lodash";
 import { EventEmitter } from "stream";
-import { ICommonTagsResult, parseFile as parseMetadataFromFile } from "music-metadata";
+import { IAudioMetadata, ICommonTagsResult, parseFile as parseMetadataFromFile } from "music-metadata";
 import { compareTwoStrings } from "string-similarity";
 import type TypedEventEmitter from "typed-emitter";
 import { DeckListener, Medley, PreQueueListener, Queue } from "@medley/medley";
 import { Crate, CrateSequencer } from "./crate";
 import { Track } from "./track";
+import { TrackCollection } from "./collections";
+import { Promise as NodeID3 } from 'node-id3';
 
-export type BoomBoxMetadata = ICommonTagsResult;
+type Rotation = 'normal' | 'request';
+
+export type BoomBoxMetadata = {
+  tags?: ICommonTagsResult;
+  rotation: Rotation;
+  priority?: number;
+};
 
 export interface BoomBoxEvents {
   sequenceChange: (activeCrate: Crate<BoomBoxMetadata>) => void;
   currentCrateChange: (oldCrate: Crate<BoomBoxMetadata>, newCrate: Crate<BoomBoxMetadata>) => void;
   trackQueued: (track: Track<BoomBoxMetadata>) => void;
   trackLoaded: (track: Track<BoomBoxMetadata>) => void;
-  trackStarted: (track: Track<BoomBoxMetadata>) => void;
+  trackStarted: (track: Track<BoomBoxMetadata>, lastTrack?: Track<BoomBoxMetadata>) => void;
   error: (error: Error) => void;
 }
 
@@ -45,6 +53,7 @@ type BoomBoxOptions = {
   duplicationSimilarity?: number;
 
 }
+
 export class BoomBox extends (EventEmitter as new () => TypedEventEmitter<BoomBoxEvents>) {
   readonly sequencer: CrateSequencer<BoomBoxMetadata>;
 
@@ -76,8 +85,74 @@ export class BoomBox extends (EventEmitter as new () => TypedEventEmitter<BoomBo
     this.sequencer.on('change', (crate: Crate) => this.emit('sequenceChange', crate as unknown as Crate<BoomBoxMetadata>));
   }
 
+  private _currentCrate: Crate<BoomBoxMetadata> | undefined;
+  private _currentTrack: Track<BoomBoxMetadata> | undefined = undefined;
+  private _nextTrack: Track<BoomBoxMetadata> | undefined;
 
-  private currentCrate: Crate<BoomBoxMetadata> | undefined;
+  get crate() {
+    return this._currentCrate;
+  }
+
+  get track() {
+    return this._currentTrack;
+  }
+
+  get nextTrack() {
+    return this._nextTrack;
+  }
+
+  private async getMusicMetadata(path: string): Promise<IAudioMetadata | undefined> {
+    try {
+      const result = await parseMetadataFromFile(path);
+      const { common, format: { tagTypes = [] }} = result;
+      const hasLyrics = common.lyrics?.length === 1;
+
+      if (hasLyrics) {
+        return result;
+      }
+
+      // No lyrics detcted, or mis-interpreted
+      // music-metadata does not map TXXX:LYRICS into the lyrics field
+
+      // Try looking up from ID3v2
+      const id3Types = _.intersection(tagTypes, ['ID3v2.3', 'ID3v2.4']);
+      for (const tagType of id3Types) {
+        const tags = result.native[tagType];
+
+        const uslt = tags.find(t => t.id === 'USLT');
+        if (uslt) {
+          const value = _.get(uslt, 'value.text');
+          if (value) {
+            result.common.lyrics = [value];
+            break;
+          }
+        }
+
+        const lyricTags = tags.filter(t => t.id === 'TXXX:LYRICS');
+        if (lyricTags.length === 1) {
+          const { value } = lyricTags[0];
+          if (value) {
+            result.common.lyrics = [value];
+            break;
+          }
+        }
+
+        // This is rare: Although, TXXX:LYRICS was found, but somehow music-metadata read it incorrectly, where it tries to split tag value by a slash
+        // We will use node-id3 to extract TXXX instead
+        const { userDefinedText: customTags } = await NodeID3.read(path, { include: ['TXXX'] });
+        const foundTag = customTags?.find(t => t.description === 'LYRICS');
+
+        if (foundTag) {
+          result.common.lyrics = [foundTag.value];
+        }
+      }
+
+      return result;
+    }
+    catch {
+
+    }
+  }
 
   private isTrackLoadable = async (path: string) => this.medley.isTrackLoadable(path);
 
@@ -87,14 +162,17 @@ export class BoomBox extends (EventEmitter as new () => TypedEventEmitter<BoomBo
     }
 
     try {
-      const metadata = await parseMetadataFromFile(path);
-      const playedArtists = flatten(this.artistHistory).map(_.toLower);
-      const currentArtists = getArtists(metadata.common).map(_.toLower);
-      const dup = some(playedArtists, a => some(currentArtists, b => compareTwoStrings(a, b) >= this.options.duplicationSimilarity));
-      if (dup) {
-        console.log('Skipping', path, 'because of artist duplication');
+      const musicMetadata = await this.getMusicMetadata(path);
+
+      if (!musicMetadata) {
+        return { rotation: 'normal' };
       }
-      return !dup ? metadata.common : false;
+
+      const boomBoxMetadata: BoomBoxMetadata = { tags: musicMetadata.common, rotation: 'normal' };
+      const playedArtists = flatten(this.artistHistory).map(_.toLower);
+      const currentArtists = getArtists(boomBoxMetadata).map(_.toLower);
+      const dup = some(playedArtists, a => some(currentArtists, b => compareTwoStrings(a, b) >= this.options.duplicationSimilarity));
+      return !dup ? boomBoxMetadata : false;
     }
     catch {
 
@@ -103,7 +181,6 @@ export class BoomBox extends (EventEmitter as new () => TypedEventEmitter<BoomBo
     return true;
   }
 
-  private nextTrack: Track<BoomBoxMetadata> | undefined;
 
   private preQueue: PreQueueListener = async (done) => {
     try {
@@ -114,16 +191,16 @@ export class BoomBox extends (EventEmitter as new () => TypedEventEmitter<BoomBo
         return;
       }
 
-      this.nextTrack = nextTrack;
+      this._nextTrack = nextTrack;
       this.queue.add(nextTrack);
 
-      if (this.currentCrate !== nextTrack.crate) {
+      if (this._currentCrate !== nextTrack.crate) {
 
-        if (this.currentCrate && nextTrack.crate) {
-          this.emit('currentCrateChange', this.currentCrate, nextTrack.crate);
+        if (this._currentCrate && nextTrack.crate) {
+          this.emit('currentCrateChange', this._currentCrate, nextTrack.crate);
         }
 
-        this.currentCrate = nextTrack.crate;
+        this._currentCrate = nextTrack.crate;
       }
 
       this.emit('trackQueued', nextTrack);
@@ -138,19 +215,25 @@ export class BoomBox extends (EventEmitter as new () => TypedEventEmitter<BoomBo
   }
 
   private deckLoaded: DeckListener = (deck) => {
-    if (this.nextTrack) {
-      this.emit('trackLoaded', this.nextTrack);
+    if (this._nextTrack) {
+      this.emit('trackLoaded', this._nextTrack);
     }
   }
 
   private deckStarted: DeckListener = (deck) => {
-    if (this.nextTrack) {
-      this.emit('trackStarted', this.nextTrack);
+    if (this._nextTrack) {
+      const newTrack = this._nextTrack;
+      const lastTrack = this._currentTrack;
+
+      this._currentTrack = newTrack;
+      this._nextTrack = undefined;
+
+      this.emit('trackStarted', newTrack, lastTrack);
 
       const { noDuplicatedArtist } = this.options;
 
       if (noDuplicatedArtist > 0) {
-        const { metadata } = this.nextTrack;
+        const { metadata } = newTrack;
         if (metadata) {
           this.artistHistory.push(getArtists(metadata));
 
@@ -161,7 +244,7 @@ export class BoomBox extends (EventEmitter as new () => TypedEventEmitter<BoomBo
       }
     }
 
-    this.nextTrack = undefined;
+    this._nextTrack = undefined;
   }
 
   get crates() {
@@ -174,8 +257,12 @@ export class BoomBox extends (EventEmitter as new () => TypedEventEmitter<BoomBo
 }
 
 function getArtists(metadata: BoomBoxMetadata): string[] {
-  const { artist } = metadata;
-  const artists = (metadata.artists ?? []);
+  if (!metadata.tags) {
+    return [];
+  }
+
+  const { artist, artists = [] } = metadata.tags;
+
   if (artist) {
     artists.push(artist);
   }
