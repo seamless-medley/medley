e2898677e42a08963505972e1fdbfdfefdf1d39e
Wittawas Nakkasem <vittee@hotmail.com>
2022-03-03 12:44:13 +0700

    Refactor the "join" command

--- packages/discord/src/bot/command/commands/join.ts
+++ packages/discord/src/bot/command/commands/join.ts
@@ -1,6 +1,7 @@
 import { CommandInteraction, MessageEmbed, Permissions } from "discord.js";
 import { ChannelType, CommandDescriptor, InteractionHandlerFactory, OptionType, SubCommandLikeOption } from "../type";
-import { deny, permissionGuard, reply } from "../utils";
+import { deny, guildIdGuard, HighlightTextType, makeHighlightedMessage, permissionGuard, reply, warn } from "../utils";
+import { createStationSelector } from "./tune";
 
 const declaration: SubCommandLikeOption = {
   type: OptionType.SubCommand,
@@ -32,33 +33,68 @@ const createCommandHandler: InteractionHandlerFactory<CommandInteraction> = (aut
   const channelToJoin = automaton.client.channels.cache.get(channel.id);
 
   if (!channelToJoin?.isVoice()) {
+    deny(interaction, 'Cannot join non-voice channel');
+    return;
+  }
+
+  const guildId = guildIdGuard(interaction);
+
+  const state = automaton.getGuildState(guildId);
+
+  if (state?.voiceChannelId === channelToJoin.id) {
+    warn(interaction, 'Already joined');
     return;
   }
 
   await reply(interaction, `Joining ${channelToJoin}`);
 
+  const createEmbed = () => {
+    const stationName = state?.stationLink?.station?.name;
+
+    const embed = new MessageEmbed()
+      .setColor('RANDOM')
+      .setTitle('Joined')
+      .addField('Channel', channel?.toString());
+
+    if (stationName) {
+      embed.addField('Station', stationName);
+    }
+
+    return embed;
+  }
+
   try {
     const result = await automaton.join(channelToJoin);
 
-    if (result === 'joined') {
+    if (result.status === 'joined') {
       reply(interaction, {
         content: null,
-        embeds: [
-          new MessageEmbed()
-            .setColor('RANDOM')
-            .setTitle('Joined')
-            .addField('channel', channel?.toString())
-        ]
+        embeds: [createEmbed()]
       });
 
       return;
     }
 
-    // TODO: Handle no_station, show options for tuning
-    deny(
-      interaction,
-      result === 'no_station' ? 'No station was tuned' : ''
-    )
+    if (result.status === 'no_station') {
+      createStationSelector(automaton, interaction, async (tuned) => {
+        if (tuned) {
+          // Rejoin
+          if ((await automaton.join(channelToJoin)).status !== 'joined') {
+            interaction.followUp(makeHighlightedMessage('Could not tune and join', HighlightTextType.Red));
+            return;
+          }
+        }
+
+        interaction.followUp({
+          content: null,
+          embeds: [createEmbed()]
+        });
+      });
+
+      return;
+    }
+
+    deny(interaction, 'Could not join, error establishing a voice connection');
   }
   catch (e) {
     console.error(e);
