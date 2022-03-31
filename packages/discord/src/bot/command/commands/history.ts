b1d53cfad04dc844c9e349a1cfb7bc986c00346c
Wittawas Nakkasem <vittee@hotmail.com>
2022-04-01 02:13:34 +0700

    Tweak the history command, to use Discord time formatting

--- packages/discord/src/bot/command/commands/history.ts
+++ packages/discord/src/bot/command/commands/history.ts
@@ -9,12 +9,6 @@ const declaration: SubCommandLikeOption = {
   description: 'Show track history'
 }
 
-const timeFormatter = Intl.DateTimeFormat([], { hourCycle: 'h24', hour: '2-digit', minute: '2-digit', second: '2-digit' });
-const tzFormatter = Intl.DateTimeFormat([], { timeZoneName: 'short' });
-
-const formatTime = (date: Date) => timeFormatter.formatToParts(date).map(p => p.value).join('');
-const formatTz = (date: Date) => tzFormatter.formatToParts(date).find(p => p.type === 'timeZoneName')?.value;
-
 const createCommandHandler: InteractionHandlerFactory<CommandInteraction> = (automaton) => async (interaction) => {
   const { station } = guildStationGuard(automaton, interaction);
 
@@ -29,7 +23,8 @@ const createCommandHandler: InteractionHandlerFactory<CommandInteraction> = (aut
 
   for (const { playedTime, trackPlay: { track }  } of history) {
     const banner = getTrackBanner(track);
-    const line = `**[**\`${formatTime(playedTime)}\`**]**: ${banner}`;
+
+    const line = `> **[**<t:${Math.trunc(playedTime.valueOf() / 1000)}:T>**]**: ${banner}`;
 
     if (length + line.length >= 1000) {
       break;
@@ -39,11 +34,8 @@ const createCommandHandler: InteractionHandlerFactory<CommandInteraction> = (aut
     length += line.length;
   }
 
-  const tz = formatTz(new Date);
-  const timeZoneInfo = tz ? ` (Time displayed in ${tz} time zone)` : '';
-
   interaction.reply([
-    `**Track History**${timeZoneInfo}`,
+    `**Tracks History**`,
     '',
     ...lines
   ].join('\n'));
