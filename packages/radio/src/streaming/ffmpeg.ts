a3ae7ec21e70450afe59937e4b0cd335529cf673
Wittawas Nakkasem <vittee@hotmail.com>
2022-04-12 01:38:40 +0700

    Add FFmpeg Overseer

--- packages/radio/src/streaming/ffmpeg.ts
+++ packages/radio/src/streaming/ffmpeg.ts
@@ -9,3 +9,44 @@ export const spawnFFmpeg = (args: string[]): FFmpegChildProcess => spawn(
   ['-loglevel', '0'].concat(args),
   { stdio: [ null, null, 'ignore' ] }
 );
+
+export type FFmpegOverseerOptions = {
+  args: string[];
+  afterSpawn?: (overseer: FFmpegOverseer) => Promise<void>;
+}
+
+export type FFmpegOverseer = {
+  process: FFmpegChildProcess;
+  respawn: () => Promise<void>;
+  kill: () => void;
+}
+
+export async function createFFmpegOverseer(options: FFmpegOverseerOptions): Promise<FFmpegOverseer> {
+  let process: FFmpegChildProcess;
+
+  function kill() {
+    if (process && !process.killed) {
+      process?.kill();
+    }
+  }
+
+  async function spawn() {
+    kill();
+
+    process = spawnFFmpeg(options.args);
+
+    await options.afterSpawn?.({
+      process,
+      respawn: spawn,
+      kill
+    });
+  }
+
+  await spawn();
+
+  return {
+    process: process!,
+    respawn: spawn,
+    kill
+  }
+}
