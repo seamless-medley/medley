df38144e77f263e9902a6f15699e4da82133521f
Wittawas Nakkasem <vittee@hotmail.com>
2022-12-15 00:00:50 +0700

    Clean up

--- packages/radio/src/discord/sample_main.ts
+++ packages/radio/src/discord/sample_main.ts
@@ -25,7 +25,7 @@ type StoredConfig = {
 
 const moods = {
   bright: ['bright'],
-  up: ['upbeat', 'groovy'],
+  up: ['upbeat', 'groovy', 'joyful'],
   easy: ['lovesong', 'chill'],
   sad: ['lonely', 'brokenhearted', 'hurt']
 }
@@ -38,32 +38,33 @@ const musicCollections: MusicLibraryDescriptor[] = [
   { id: 'hurt', description:'Hurt', path: 'D:\\vittee\\Google Drive\\musics\\hurt' },
   { id: 'lonely', description:'Lonely', path: 'D:\\vittee\\Google Drive\\musics\\lonely' },
   { id: 'lovesong', description:'Love Song', path: 'D:\\vittee\\Google Drive\\musics\\lovesong' },
+  { id: 'joyful', description:'Joyful', path: 'D:\\vittee\\Google Drive\\musics\\joyful' },
   { id: 'upbeat', description:'Upbeat', path: 'D:\\vittee\\Google Drive\\musics\\upbeat' },
   { id: 'new-released', description:'New Released', path: 'D:\\vittee\\Google Drive\\musics\\new-released', disableLatch: true, noFollowOnRequest: true },
-  { id: 'thai', auxiliary: true, description:'Thai', path: 'M:\\Repository\\th' },
   { id: 'thai', auxiliary: true, description:'Thai', path: 'M:\\Repository\\th' },
   { id: 'inter', auxiliary: true, description:'International', path: 'M:\\Repository\\inter' },
 ];
 
 const sequences: SequenceConfig[] = [
-  { crateId: 'guid1', collections: [ { id: 'new-released' }], limit: { by: 'one-of', list: [1, 1, 1, 2] } },
-  { crateId: 'guid2', collections: [ { id: 'bright' }], limit: { by: 'upto', upto: 2 } },
-  { crateId: 'guid3', collections: [ { id: 'groovy' }], chance: [1, 3], limit: 1 },
-  { crateId: 'guid4', collections: [ { id: 'upbeat' }], chance: [2, 8], limit: { by: 'range', range: [1, 2] } },
-  { crateId: 'guid5', collections: [ { id: 'chill' }], limit: { by: 'upto', upto: 2 } },
-  { crateId: 'guid6', collections: [ { id: 'lovesong' }], limit: { by: 'range', range: [0, 2] } },
-  { crateId: 'guid7',
+  { crateId: 'guid1', collections: [ { id: 'new-released' }], chance: 'random', limit: { by: 'one-of', list: [1, 1, 1, 2] } },
+  { crateId: 'guid2', collections: [ { id: 'bright' }], chance: [1, 2], limit: { by: 'upto', upto: 2 } },
+  { crateId: 'guid3', collections: [ { id: 'joyful' }], limit: { by: 'upto', upto: 2 } },
+  { crateId: 'guid4', collections: [ { id: 'upbeat' }], chance: [2, 6], limit: { by: 'range', range: [1, 2] } },
+  { crateId: 'guid5', collections: [ { id: 'groovy' }], chance: [1, 3], limit: 1 },
+  { crateId: 'guid7', collections: [ { id: 'chill' }], limit: { by: 'range', range: [2, 3] } },
+  { crateId: 'guid8', collections: [ { id: 'lovesong' }], limit: { by: 'upto', upto: 2 } },
+  { crateId: 'guid9',
     collections: [
       { id: 'lonely', weight: 1 },
       { id: 'brokenhearted', weight: 0.5 }
     ],
     limit: { by: 'upto', upto: 1 }
   },
-  { crateId: 'guid8', collections: [ { id: 'brokenhearted' }], limit: { by: 'range', range: [1, 2] } },
-  { crateId: 'guid8_1', collections: [ { id: 'hurt' }], chance: [1, 2], limit: { by: 'upto', upto: 1 } },
-  { crateId: 'guid9', collections: [ { id: 'lonely' }], limit: { by: 'range', range: [1, 2] } },
-  { crateId: 'guid10', collections: [ { id: 'lovesong' }], limit: { by: 'upto', upto: 2 } },
-  { crateId: 'guid11', collections: [ { id: 'chill' }], chance: [1, 1], limit: { by: 'upto', upto: 2 } }
+  { crateId: 'guid10', collections: [ { id: 'brokenhearted' }], limit: { by: 'range', range: [1, 2] } },
+  { crateId: 'guid11', collections: [ { id: 'hurt' }], chance: [1, 2], limit: { by: 'upto', upto: 1 } },
+  { crateId: 'guid12', collections: [ { id: 'lonely' }], limit: { by: 'range', range: [1, 2] } },
+  { crateId: 'guid13', collections: [ { id: 'lovesong' }], limit: { by: 'upto', upto: 2 } },
+  { crateId: 'guid14', collections: [ { id: 'chill' }], chance: [1, 1], limit: { by: 'upto', upto: 2 } }
 ];
 
 const makeSweeperRule = (type: string) => new WatchTrackCollection(type, {
@@ -95,6 +96,10 @@ const sweeperRules: SweeperInsertionRule[] = [
   },
   { // Fresh
     to: ['new-released'],
+    collection: makeSweeperRule('fresh')
+  },
+  {
+    from: ['new-released'],
     collection: makeSweeperRule('fresh')
   }
 ];
