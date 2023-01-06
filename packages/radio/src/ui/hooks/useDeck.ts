b8fd26c8deb78649822329b62a5e4f4fd4916992
Wittawas Nakkasem <vittee@hotmail.com>
2023-01-06 09:16:32 +0700

    Add useDeck hook and Deck component

---
+++ packages/radio/src/ui/hooks/useDeck.ts
@@ -0,0 +1,187 @@
+import { DeckIndex } from "@seamless-medley/core";
+import { useEffect, useState } from "react";
+import { DeckInfoWithPositions } from "../../socket/po/deck";
+import { Station } from "../../socket/remote/station";
+import { Remotable } from "../../socket/types";
+import { useClient } from "../hooks/useClient";
+
+type DeckCache = Partial<Record<DeckIndex, Promise<DeckInfoWithPositions | undefined>>>;
+
+const cache = new WeakMap<Remotable<Station>, DeckCache>();
+
+const ensureCache = (station: Remotable<Station>) => {
+  if (!cache.has(station)) {
+    cache.set(station, {});
+  }
+}
+
+const storeCache = (station: Remotable<Station>, index: DeckIndex, info?: Promise<DeckInfoWithPositions | undefined>) => {
+  ensureCache(station);
+  cache.get(station)![index] = info;
+}
+
+const getCache = (station: Remotable<Station>, index: DeckIndex) => {
+  return cache.get(station)?.[index];
+}
+
+export function useDeck(station: Remotable<Station> | undefined, index: DeckIndex) {
+  const client = useClient();
+
+  const [info, setInfo] = useState<DeckInfoWithPositions | undefined>();
+  const [cover, setCover] = useState<string | undefined>();
+
+  // TODO: Parse Lyrics
+  const [lyrics, setLyrics] = useState<string | undefined>();
+
+  const update = (deckIndex: number, newInfo: DeckInfoWithPositions) => {
+    if (deckIndex !== index) {
+      return;
+    }
+
+    setInfo(newInfo);
+
+    const {
+      cover,
+      coverMimeType,
+      lyrics
+    } = newInfo?.trackPlay?.track?.extra?.coverAndLyrics ?? {};
+
+    setCover(cover && coverMimeType ? URL.createObjectURL(new Blob([cover])) : undefined);
+    setLyrics(lyrics);
+
+    return newInfo;
+  }
+
+  const refreshDeckInfo = () => {
+    if (!station) {
+      return Promise.resolve(undefined);
+    }
+
+    const promise = station.getDeckInfo(index).then(newInfo => update(index, newInfo));
+    storeCache(station, index, promise);
+    return promise;
+  }
+
+  const onLoaded: Station['ϟdeckLoaded'] = async (deckIndex, newInfo) => {
+    if (!station) {
+      return;
+    }
+
+    if (deckIndex !== index) {
+      return;
+    }
+
+    storeCache(station, deckIndex, Promise.resolve(update(deckIndex, newInfo)));
+  }
+
+  const onStarted: Station['ϟdeckStarted'] = async (deckIndex, positions) => {
+    if (!station) {
+      return;
+    }
+
+    if (deckIndex !== index) {
+      return;
+    }
+
+    const cached = await getCache(station, deckIndex);
+
+    if (!cached) {
+      return;
+    }
+
+    const newInfo = {
+      ...cached,
+      playing: true,
+      positions
+    }
+
+    setInfo(newInfo);
+    storeCache(station, index, Promise.resolve(newInfo));
+  }
+
+  const onActive: Station['ϟdeckActive'] = async (deckIndex, positions) => {
+    if (!station) {
+      return;
+    }
+
+    if (deckIndex !== index) {
+      return;
+    }
+
+    const cached = await getCache(station, deckIndex);
+
+    if (!cached) {
+      return;
+    }
+
+    const newInfo = {
+      ...cached,
+      playing: true,
+      active: true,
+      positions
+    }
+
+    setInfo(newInfo);
+    storeCache(station, deckIndex, Promise.resolve(newInfo));
+  }
+
+  const onUnloaded: Station['ϟdeckUnloaded'] = async (deckIndex) => {
+    if (deckIndex !== index) {
+      return;
+    }
+
+    setInfo(undefined);
+    setCover(undefined);
+
+    if (!station) {
+      return;
+    }
+
+    const cached = await getCache(station, deckIndex);
+
+    if (!cached) {
+      return;
+    }
+
+    storeCache(station, deckIndex, undefined);
+  }
+
+  useEffect(() => {
+    client.on('connect', refreshDeckInfo);
+
+    return () => {
+      client.off('connect', refreshDeckInfo);
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!station) {
+      return;
+    }
+
+    (getCache(station, index) ?? refreshDeckInfo()).then(info => {
+      if (info) {
+        update(index, info);
+      }
+    });
+
+    station.on('deckLoaded', onLoaded);
+    station.on('deckUnloaded', onUnloaded);
+    station.on('deckStarted', onStarted);
+    station.on('deckActive', onActive);
+
+    return () => {
+      station.off('deckLoaded', onLoaded);
+      station.off('deckUnloaded', onUnloaded);
+      station.off('deckStarted', onStarted);
+      station.off('deckActive', onActive);
+    }
+
+  }, [station, index]);
+
+  return {
+    info,
+    cover,
+    lyrics
+  }
+}
--- packages/radio/src/ui/main.tsx
+++ packages/radio/src/ui/main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import { initRoot } from './init';
 import { Button, Group, MantineProvider } from '@mantine/core';
@@ -6,9 +6,12 @@ import { StubOf } from '../socket/stub';
 import { noop } from 'lodash';
 import { useSurrogateWithRemotable } from './hooks/surrogate';
 import { Station } from '../socket/remote/station';
-import { TrackPlay } from '../socket/po/track';
+import { Deck } from './components';
 
 const StubStation = StubOf<Station>(class Station {
+  id = undefined as any;
+  name = undefined as any;
+  description = undefined as any;
   playing = undefined as any;
   paused = undefined as any;
   playState = undefined as any;
@@ -16,33 +19,12 @@ const StubStation = StubOf<Station>(class Station {
   start = noop as any;
   pause = noop as any;
   skip = noop as any;
+  getDeckPositions = noop as any;
+  getDeckInfo = noop as any;
 });
 
 const App: React.FC = () => {
-  const [trackPlay, setTrackPlay] = useState<TrackPlay | undefined>();
-  const [coverURL, setCoverURL] = useState<string | undefined>();
-
   const [station, stationProps] = useSurrogateWithRemotable(StubStation, 'station', 'default');
-
-  const handleTrackStarted = (deckIndex: number, trackPlay: TrackPlay) => {
-    setTrackPlay(trackPlay);
-
-    const cover = trackPlay.track.extra?.coverAndLyrics?.cover;
-    const blob = new Blob(cover ? [cover] : []);
-    setCoverURL(URL.createObjectURL(blob));
-  }
-
-  useEffect(() => {
-    if (!station) {
-      return;
-    }
-
-    station.on('trackStarted', handleTrackStarted);
-
-    return () => {
-      station.off('trackStarted', handleTrackStarted);
-    }
-  }, [station]);
 
   return (
     <>
@@ -52,10 +34,15 @@ const App: React.FC = () => {
         <Button disabled={!station} onClick={() => console.log('Skip', station?.skip())} color="red">Skip</Button>
       </Group>
       <h4>Play State: { stationProps?.playState }</h4>
-      <img src={coverURL} />
-      <pre>
-        {JSON.stringify(trackPlay, undefined, 2)}
-      </pre>
+
+      <h2>Deck1</h2>
+      <Deck station={station} index={0} />
+
+      <h2>Deck2</h2>
+      <Deck station={station} index={1} />
+
+      <h2>Deck3</h2>
+      <Deck station={station} index={2} />
     </>
   );
 }
