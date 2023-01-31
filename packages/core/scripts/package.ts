d3861d29e5394a50ce8f26bb5252022ca5348b02
Wittawas Nakkasem <vittee@hotmail.com>
2023-02-01 02:30:01 +0700

    Update build scripts

--- packages/core/scripts/package.ts
+++ packages/core/scripts/package.ts
@@ -1,23 +1,17 @@
 import { PackageJson } from 'type-fest';
+import { mapValues, omit } from 'lodash';
 import mkdirp from 'mkdirp';
 import fs from 'fs';
 
 async function transform() {
   const p = require('../package.json') as Required<PackageJson>;
 
-  p.main = 'index.js';
-  p.types = 'index.d.ts';
-
-  p.scripts = {
-    install: p.scripts.install
-  }
-
-  delete p.gypfile;
-  delete (p as any).devDependencies;
+  p.main = p.main.replace(/src\/(.*).ts$/, '$1.js');
+  p.dependencies = mapValues(p.dependencies, v => v?.replace(/workspace:../, '..'));
 
   await mkdirp('dist');
 
-  return JSON.stringify(p, null, 2);
+  return JSON.stringify(omit(p, 'scripts', 'devDependencies'), null, 2);
 }
 
 transform().then(s => fs.createWriteStream('dist/package.json').write(s));
