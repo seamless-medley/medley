d2810633780716e12affa29e4b98e102d85bac8a
Wittawas Nakkasem <vittee@hotmail.com>
2023-03-13 23:56:15 +0700

    Refactor vote command

--- packages/radio/src/discord/command/commands/vote.ts
+++ packages/radio/src/discord/command/commands/vote.ts
@@ -1,9 +1,9 @@
-import { Audience, AudienceType, getTrackBanner, makeAudience, StationRequestedTrack, StationTrack, TrackPeek, TrackWithRequester } from "@seamless-medley/core";
-import { CommandInteraction, Message, EmbedBuilder, MessageReaction, } from "discord.js";
-import { chain, isEqual, keyBy, sampleSize, take } from "lodash";
+import { AudienceType, getTrackBanner, makeAudience, StationRequestedTrack, TrackPeek } from "@seamless-medley/core";
+import { CommandInteraction, Message, EmbedBuilder, MessageReaction, ActionRowBuilder, MessageActionRowComponentBuilder, ButtonBuilder, ButtonStyle, } from "discord.js";
+import { chain, isEqual, keyBy, sampleSize, take, without } from "lodash";
 import * as emojis from "../../emojis";
 import { CommandDescriptor, InteractionHandlerFactory, OptionType, SubCommandLikeOption } from "../type";
-import { formatMention, guildStationGuard, makeRequestPreview, warn } from "../utils";
+import { formatMention, guildStationGuard, joinStrings, makeRequestPreview, reply, warn } from "../utils";
 
 const declaration: SubCommandLikeOption = {
   type: OptionType.SubCommand,
@@ -13,12 +13,13 @@ const declaration: SubCommandLikeOption = {
 
 type Nominatee = TrackPeek<StationRequestedTrack> & {
   banner: string;
-  votes: number,
-  // TODO: voters
+  votes: number;
   emoji: string;
 }
 
 const guildVoteMessage = new Map<string, Message>();
+
+const distinguishableEmojis = without(emojis.distinguishable, '🏁');
 
 const createCommandHandler: InteractionHandlerFactory<CommandInteraction> = (automaton) => async (interaction) => {
   const { guildId, station } = guildStationGuard(automaton, interaction);
@@ -29,7 +30,9 @@ const createCommandHandler: InteractionHandlerFactory<CommandInteraction> = (aut
     return;
   }
 
-  const count = Math.min(emojis.distinguishable.length, station.requestsCount, 20);
+
+
+  const count = Math.min(distinguishableEmojis.length, station.requestsCount, 20);
   const peekings = station.peekRequests(0, count);
 
   if (peekings.length <= 1) {
@@ -42,12 +45,16 @@ const createCommandHandler: InteractionHandlerFactory<CommandInteraction> = (aut
     return;
   }
 
-  const collectibleEmojis = sampleSize(emojis.distinguishable, emojis.distinguishable.length);
+  // TODO: Add try-catch here, unlockRequests
+  const issuer = interaction.user.id;
+
+  const collectibleEmojis = sampleSize(distinguishableEmojis, distinguishableEmojis.length);
 
   const nominatees = peekings.map<Nominatee>((p, i) => ({
     ...p,
     banner: getTrackBanner(p.track),
     votes: 0,
+    voters: [],
     emoji: collectibleEmojis[i]
   }));
 
@@ -55,21 +62,32 @@ const createCommandHandler: InteractionHandlerFactory<CommandInteraction> = (aut
     nominatees.map(n => [n.emoji, n])
   );
 
+  const ttl = 90_000;
 
-  // TODO: Show time out
-  const createMessageContent = () =>
-    chain(nominatees)
-      .sortBy(
-        ({ votes, track: { priority = 0 }}) => -(votes + priority),
-        b => (b.track.lastRequestTime || 0)
-      )
-      .map(peek => previewTrack(peek))
-      .join('\n')
-      .value();
+  const getTimeout = () => `Vote Timeout: <t:${Math.trunc((Date.now() + ttl) / 1000)}:R>`;
 
+  const createMessageContent = () => chain(nominatees)
+    .sortBy(
+      ({ votes, track: { priority = 0 }}) => -(votes + priority),
+      b => (b.track.lastRequestTime || 0)
+    )
+    .map(peek => '> ' + previewTrack(peek))
+    .join('\n')
+    .value();
 
-  const message = await interaction.reply({
-    content: createMessageContent(),
+  const message = await reply(interaction, {
+    content: joinStrings([
+      getTimeout(),
+      createMessageContent(),
+    ]),
+    components: [
+      new ActionRowBuilder<MessageActionRowComponentBuilder>()
+      .addComponents(new ButtonBuilder()
+      .setCustomId('vote:end')
+      .setLabel('End')
+      .setStyle(ButtonStyle.Danger)
+      .setEmoji('🏁')),
+    ],
     embeds: [
       new EmbedBuilder()
         .setTitle('Vote')
@@ -86,19 +104,23 @@ const createCommandHandler: InteractionHandlerFactory<CommandInteraction> = (aut
     guildVoteMessage.set(guildId, msg);
 
     function updateMessage() {
+      reactionCollector.resetTimer({ time: ttl });
+
       msg.edit({
-        content: createMessageContent()
+        content: joinStrings([
+          getTimeout(),
+          createMessageContent(),
+        ])
       })
     }
 
-    const handleCollect = ({ emoji, count }: MessageReaction) => {
+    const handleCollect = (reaction: MessageReaction) => {
+      const { emoji, count } = reaction;
+
       if (emoji.name) {
         const nominatee = emojiToNominateeMap.get(emoji.name);
 
-        // TODO: If no nominatee for this emoji, completely remove this reaction (if has MANAGE MESSAGE permission)
-
         if (nominatee) {
-
           nominatee.votes = count - 1;
           updateMessage();
         }
@@ -128,79 +150,104 @@ const createCommandHandler: InteractionHandlerFactory<CommandInteraction> = (aut
 
     station.on('requestTrackAdded', handleNewRequest);
 
-    const collector = message.createReactionCollector({
+    const componentCollector = message.createMessageComponentCollector({ dispose: true, time: ttl });
+
+    componentCollector.on('collect', async (collected) => {
+      const { customId, user } = collected;
+
+      if (user.id !== issuer) {
+        collected.reply({
+          content: `Sorry, Only ${formatMention('user', issuer)} can end this vote`,
+          ephemeral: true
+        });
+        return;
+      }
+
+      // End button
+      if (customId === 'vote:end') {
+        reactionCollector.stop('end');
+        return;
+      }
+    });
+
+    const reactionCollector = message.createReactionCollector({
       dispose: true,
-      time: 30_000 // TODO: from option + peekings.length in seconds
+      time: ttl
     });
 
     for (const emoji of take(collectibleEmojis, peekings.length)) {
       await message.react(emoji!);
     }
 
-    // TODO: End vote button
+    reactionCollector.on('collect', handleCollect);
+    reactionCollector.on('remove', handleCollect);
+    reactionCollector.on('end', async (collected, reason) => {
+      console.log('Reaction colelctor on end', reason);
+      try {
+        station.off('requestTrackAdded', handleNewRequest);
+        guildVoteMessage.delete(guildId);
 
-    collector.on('collect', handleCollect);
-    collector.on('remove', handleCollect);
-    collector.on('end', async (collected) => {
-      station.off('requestTrackAdded', handleNewRequest);
-      guildVoteMessage.delete(guildId);
+        const peeks = station.peekRequests(0, nominatees.length);
+        const requestsKeyed = keyBy(peeks, n => n.track.rid);
 
-      const peeks = station.peekRequests(0, nominatees.length);
-      const requestsKeyed = keyBy(peeks, n => n.track.rid);
+        let contributors: string[] = [];
 
-      let contributors: string[] = [];
+        for (const nom of nominatees) {
+          const request = requestsKeyed[nom.track.rid];
+          if (request) {
+            const { priority = 0 } = request.track;
+            request.track.priority = priority + nom.votes;
+            //
+            const reaction = collected.get(nom.emoji);
+            if (reaction) {
+              const { users } = reaction;
+              const userIds = [...users.cache.keys()];
 
-      for (const nom of nominatees) {
-        const request = requestsKeyed[nom.track.rid];
-        if (request) {
-          const { priority = 0 } = request.track;
-          request.track.priority = priority + nom.votes;
-          //
-          const reaction = collected.get(nom.emoji);
-          if (reaction) {
-            const { users } = reaction;
-            const userIds = [...users.cache.keys()];
+              contributors = contributors.concat(userIds);
 
-            contributors = contributors.concat(userIds);
-
-            request.track.requestedBy = chain(request.track.requestedBy)
-              .concat(userIds.map(id => makeAudience(AudienceType.Discord, { automatonId: automaton.id, guildId }, id)))
-              .uniqWith(isEqual)
-              .reject(audience => audience.id === automaton.client.user!.id)
-              .value();
+              request.track.requestedBy = chain(request.track.requestedBy)
+                .concat(userIds.map(id => makeAudience(AudienceType.Discord, { automatonId: automaton.id, guildId }, id)))
+                .uniqWith(isEqual)
+                .reject(audience => audience.id === automaton.client.user!.id)
+                .value();
+            }
           }
         }
+
+        station.sortRequests();
+
+        const preview = await makeRequestPreview(station, 0, undefined, nominatees.length) || [];
+        const contributorMentions = chain(contributors)
+          .uniq()
+          .without(automaton.client.user!.id)
+          .map(id => formatMention('user', id))
+          .value();
+
+        const embed = new EmbedBuilder()
+          .setTitle('Vote Results')
+          .setColor('Random');
+
+        if (contributorMentions.length) {
+          embed.addFields({ name: 'Contributed by', value: contributorMentions.join(' ')});
+        }
+
+        await msg.reply({
+          embeds: [
+            embed
+          ],
+          content: preview.join('\n')
+        });
+
+        componentCollector.stop();
+
+        if (msg.deletable) {
+          await msg.delete();
+        }
       }
-
-      station.sortRequests();
-
-      const preview = await makeRequestPreview(station, 0, undefined, nominatees.length) || [];
-      const contributorMentions = chain(contributors)
-        .uniq()
-        .without(automaton.client.user!.id)
-        .map(id => formatMention('user', id))
-        .value();
-
-      const embed = new EmbedBuilder()
-        .setTitle('Vote Results')
-        .setColor('Random');
-
-      if (contributorMentions.length) {
-        embed.addFields({ name: 'Contributed by', value: contributorMentions.join(' ')});
+      finally {
+        station.unlockRequests(interaction.guildId);
       }
-
-      await msg.reply({
-        embeds: [
-          embed
-        ],
-        content: preview.join('\n')
-      });
-
-      await msg.delete();
-
-      // TODO: Should be in 'finally' block
-      station.unlockRequests(interaction.guildId);
-    })
+    });
   }
 }
 
