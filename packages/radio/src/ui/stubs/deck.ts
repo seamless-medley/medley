2e78b5d8bb23fb36ec9de6f92064b2b8c9fcfec5
Wittawas Nakkasem <vittee@hotmail.com>
2023-07-15 22:59:05 +0700

    chore(radio): add Deck stub

---
+++ packages/radio/src/ui/stubs/deck.ts
@@ -0,0 +1,30 @@
+import { Deck } from "../../socket/remote";
+import { StubOf } from "../../socket/stub";
+
+export const StubDeck = StubOf<Deck>(class StubbingDeck {
+  active = undefined as any;
+
+  playing = undefined as any;
+
+  trackPlay = undefined as any;
+
+  cp = undefined as any;
+
+  current = undefined as any;
+
+  duration = undefined as any;
+
+  first = undefined as any;
+
+  last = undefined as any;
+
+  leading = undefined as any;
+
+  trailing = undefined as any;
+
+  cuePoint = undefined as any;
+
+  transitionStart = undefined as any;
+
+  transitionEnd = undefined as any;
+});
