9fa99f062bc3ac285e140b54093af4069d65ece4
Wittawas Nakkasem <vittee@hotmail.com>
2023-10-23 05:51:07 +0700

    chore(radio): clean up

--- packages/radio/src/remotes/index.ts
+++ packages/radio/src/remotes/index.ts
@@ -2,12 +2,6 @@ import { Station } from "./core/station";
 import { Deck } from "./core/deck";
 import { Collection } from "./core/collection";
 
-export type {
-  Station,
-  Deck,
-  Collection
-}
-
 export interface RemoteTypes {
   station: Station;
   deck: Deck;
