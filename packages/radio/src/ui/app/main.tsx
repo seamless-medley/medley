d9edb4730586226f730ee83443f40e85d7db3fc1
Wittawas Nakkasem <vittee@hotmail.com>
2023-10-29 14:28:07 +0700

    chore: update UI dependencies

--- packages/radio/src/ui/app/main.tsx
+++ packages/radio/src/ui/app/main.tsx
@@ -1,12 +1,13 @@
 import { RouterProvider } from "@tanstack/router";
 import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 import { initRoot } from '../init';
 import { router } from "../pages/_router";
 
 initRoot().render(
   // <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS withCSSVariables>
+    <MantineProvider withCssVariables>
       <RouterProvider router={router} />
     </MantineProvider>
   // </React.StrictMode>
