3d63d65e7a4710c492ef2c77117910fa23f87c50
Wittawas Nakkasem <vittee@hotmail.com>
2023-12-07 23:30:20 +0700

    chore(radio): update combin script

--- packages/radio/scripts/combine-discord.ts
+++ packages/radio/scripts/combine-discord.ts
@@ -10,13 +10,14 @@ async function combine() {
   await emptyDir(combinePath);
 
   const copyTasks = [
+    copy('../logging', join(combinePath, 'logging'), { filter: src => !/(package.json|node_modules)$/.test(src) }),
     copy('../utils/dist', join(combinePath, 'utils'), { filter: src => !/package.json$/.test(src) }),
     copy('../core/dist', join(combinePath, 'core'), { filter: src => !/package.json$/.test(src) }),
     copy('dist', join(combinePath, 'radio'), {
       filter: (src) => {
         const [, group] = src.split(sep);
 
-        if (/^(server|ui|socket|streaming|poc-)/.test(group)) {
+        if (/^(server|ui|socket|remotes|streaming|poc-)/.test(group)) {
           return false;
         }
 
@@ -28,12 +29,13 @@ async function combine() {
   await Promise.all(copyTasks);
 
   const mainPackage = require('../package.json') as PackageJson;
+  const { dependencies: loggingDeps = {} } = (require('../../logging/package.json') as PackageJson);
+  const { dependencies: utilsDeps = {} } = require('../../utils/package.json') as PackageJson;
   const { dependencies: coreDeps = {} } = (require('../../core/package.json') as PackageJson);
-  const { dependencies: utilsDeps = {} } = require('../../utils') as PackageJson;
 
   const deps = chain({})
-    .extend(utilsDeps, coreDeps, mainPackage.dependencies)
-    .omitBy((_, name) => /^(@emotion|socket\.io|react|express|valtio|@mantine|framer-motion|ffmpeg|jotai|reflect-metadata|notepack.io|opus-decoder)/.test(name ?? ''))
+    .extend(loggingDeps, utilsDeps, coreDeps, mainPackage.dependencies)
+    .omitBy((_, name) => /^(@emotion|socket\.io|react|express|valtio|@mantine|framer-motion|ffmpeg|jotai|reflect-metadata|notepack.io|opus-decoder|mediasoup)/.test(name ?? ''))
     .transform((o, value, key) => {
       if (key.startsWith('@seamless-medley/') && value?.startsWith('..')) {
         o[key] = value.substring(1);
