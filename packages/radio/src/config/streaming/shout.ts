dc12fd3131d85c907e9846088357692ba45d824b
Wittawas Nakkasem <vittee@hotmail.com>
2024-01-05 14:09:55 +0700

    refactor(radio): move configuration types specific to Icecast supported codecs to shout adapter

--- packages/radio/src/config/streaming/shout.ts
+++ packages/radio/src/config/streaming/shout.ts
@@ -1,5 +1,86 @@
 import { z } from "zod";
-import { AacOptions, FlacOptions, HeAAcOptions, Mp3Options, OpusOptions, StreamingConfigTrait, VorbisOptions } from "./adapter";
+import { StreamingConfigTrait } from "./adapter";
+
+export const AacOptions = z.object({
+  codec: z.literal('aac'),
+  sampleRate: z.union([
+    z.literal(11025),
+    z.literal(12000),
+    z.literal(16000),
+    z.literal(22050),
+    z.literal(24000),
+    z.literal(32000),
+    z.literal(44100),
+    z.literal(48000)
+  ]),
+  bitrate: z.number().int().min(16).max(320)
+}).strict();
+
+
+export const HeAAcOptions = z.object({
+  codec: z.literal('he-aac'),
+  sampleRate: z.union([
+    z.literal(11025),
+    z.literal(12000),
+    z.literal(16000),
+    z.literal(22050),
+    z.literal(24000),
+    z.literal(32000),
+    z.literal(44100),
+    z.literal(48000)
+  ]),
+  bitrate: z.number().int().min(16).max(320)
+}).strict();
+
+export const VorbisOptions = z.object({
+  codec: z.literal('vorbis'),
+  sampleRate: z.union([
+    z.literal(8000),
+    z.literal(11025),
+    z.literal(12000),
+    z.literal(16000),
+    z.literal(22050),
+    z.literal(24000),
+    z.literal(32000),
+    z.literal(44100),
+    z.literal(48000)
+  ]),
+  bitrate: z.number().int().min(8).max(320)
+}).strict();
+
+export const OpusOptions = z.object({
+  codec: z.literal('opus'),
+  sampleRate: z.union([
+    z.literal(8000),
+    z.literal(12000),
+    z.literal(16000),
+    z.literal(24000),
+    z.literal(44100),
+    z.literal(48000)
+  ]),
+  bitrate: z.number().int().min(8).max(320)
+}).strict();
+
+export const FlacOptions = z.object({
+  codec: z.literal('flac'),
+  sampleRate: z.number().int().min(8_000).max(192_000)
+}).strict();
+
+export const Mp3Options = z.object({
+  codec: z.literal('mp3'),
+  sampleRate: z.union([
+    z.literal(8000),
+    z.literal(11025),
+    z.literal(12000),
+    z.literal(16000),
+    z.literal(22050),
+    z.literal(24000),
+    z.literal(32000),
+    z.literal(44100),
+    z.literal(48000)
+  ]),
+  bitrate: z.number().int().min(80).max(320)
+}).strict();
 
 const ShoutFormat = z.discriminatedUnion('codec', [
   AacOptions,
@@ -22,6 +103,6 @@ const IcecastConfig = z.object({
 
 export const ShoutConfig = StreamingConfigTrait.extend({
   type: z.literal('shout'),
+  format: ShoutFormat,
   icecast: IcecastConfig,
-  format: ShoutFormat
 }).strict();
