0522cc31e092762493cef205641a1ffe91878085
Wittawas Nakkasem <vittee@hotmail.com>
2024-03-19 04:16:42 +0700

    perf(ui): improve PlayDeck canvas rendering

--- packages/radio/src/ui/components/PlayDeck.tsx
+++ packages/radio/src/ui/components/PlayDeck.tsx
@@ -3,6 +3,7 @@ import type { DeckIndex, DeckPositions } from "@seamless-medley/core";
 import { useDeckInfo } from "../hooks/useDeck";
 import { Box, Image, Text } from "@mantine/core";
 import { useElementSize, useMergedRef } from "@mantine/hooks";
+import { formatDuration } from "../utils/utils";
 
 export type PlayDeckProps = {
   stationId: string | undefined;
@@ -14,66 +15,70 @@ const CanvasPlayHead: React.FC<DeckPositions> = (positions) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const ctxRef = useRef<CanvasRenderingContext2D | undefined>(undefined);
   const ref = useMergedRef(elementSize.ref, canvasRef);
+  const raf = useRef(0);
 
   useEffect(() => {
     ctxRef.current = canvasRef.current?.getContext('2d') ?? undefined;
   }, [canvasRef.current]);
 
-  useEffect(() => {
-    const ctx = ctxRef.current;
+  const ctx = ctxRef.current;
 
-    if (!ctx) {
-      return;
+  if (ctx) {
+    if (raf.current) {
+      cancelAnimationFrame(raf.current);
     }
 
-    const { duration, current, first, last, leading, trailing, cuePoint, transitionStart, transitionEnd } = positions;
+    raf.current = requestAnimationFrame(() => {
+      const { duration, current, first, last, leading, trailing, cuePoint, transitionStart, transitionEnd } = positions;
 
-    const { width: w, height: h } = ctx.canvas;
+      const { width: w, height: h } = ctx.canvas;
 
-    const positionToX = (p: number) => p / (duration ?? 0) * w;
+      const positionToX = (p: number) => p / (duration ?? 0) * w;
 
-    const drawRect = (p1: number, w: number) => ctx.fillRect(positionToX(p1), 0, positionToX(w), h);
-    const drawVertLine = (p: number) => ctx.fillRect(positionToX(p), 0, 1, h);
+      const drawRect = (p1: number, w: number) => ctx.fillRect(positionToX(p1), 0, positionToX(w), h);
+      const drawVertLine = (p: number) => ctx.fillRect(positionToX(p), 0, 1, h);
 
-    ctx.clearRect(0, 0, w, h);
+      ctx.clearRect(0, 0, w, h);
 
-    if (first !== undefined && last !== undefined) {
-      ctx.fillStyle = 'rgb(0 0 0 / 33%)'
-      drawRect(0, first); // first audible
-      drawRect(last, w - last); // last audible
-    }
+      if (first !== undefined && last !== undefined) {
+        ctx.fillStyle = 'rgb(0 0 0 / 33%)'
+        drawRect(0, first); // first audible
+        drawRect(last, w); // last audible
+      }
 
-    if (last !== undefined && transitionEnd !== undefined) {
-      ctx.fillStyle = 'rgb(0 0 0 / 88%)'
-      drawRect(transitionEnd, last - transitionEnd); // inaudible
-    }
+      if (last !== undefined && transitionEnd !== undefined) {
+        ctx.fillStyle = 'rgb(0 0 0 / 88%)'
+        drawRect(transitionEnd, last - transitionEnd); // inaudible
+      }
 
-    if (first !== undefined && current !== undefined) {
-      ctx.fillStyle = "rgb(0 255 0 / 75%)";
-      drawRect(first, current - first); // progress
-    }
+      if (first !== undefined && current !== undefined) {
+        ctx.fillStyle = "rgb(0 255 0 / 75%)";
+        drawRect(first, current - first); // progress
+      }
 
-    if (cuePoint !== undefined) {
-      ctx.fillStyle = 'blue';
-      drawVertLine(cuePoint);
-    }
+      if (cuePoint !== undefined) {
+        ctx.fillStyle = 'blue';
+        drawVertLine(cuePoint);
+      }
 
-    if (transitionStart !== undefined && transitionEnd !== undefined) {
-      ctx.fillStyle = 'rgb(255 88 100 / 60%)';
-      drawRect(transitionStart, transitionEnd - transitionStart); // transition
-    }
+      if (transitionStart !== undefined && transitionEnd !== undefined) {
+        ctx.fillStyle = 'rgb(255 88 100 / 60%)';
+        drawRect(transitionStart, transitionEnd - transitionStart); // transition
+      }
 
-    if (leading !== undefined) {
-      ctx.fillStyle = 'palevioletred';
-      drawVertLine(leading);
-    }
+      if (leading !== undefined) {
+        ctx.fillStyle = 'palevioletred';
+        drawVertLine(leading);
+      }
 
-    if (trailing !== undefined) {
-      ctx.fillStyle = 'orangered';
-      drawVertLine(trailing);
-    }
+      if (trailing !== undefined) {
+        ctx.fillStyle = 'orangered';
+        drawVertLine(trailing);
+      }
 
-  }, [ctxRef.current, positions]);
+      raf.current = 0;
+    });
+  }
 
   return (
     <canvas ref={ref} width={elementSize.width} height={elementSize.height} style={{ width: '100%', height: '100%' }}>
@@ -89,7 +94,7 @@ export const PlayDeck: React.FC<PlayDeckProps> = ({ stationId, index }) => {
     <>
       <Box style={{ height: 440, width: '100%', border: '1px solid green' }}>
         <h2>Deck{index+1}</h2>
-        <div>{info.cp.toFixed(2)} / {info.duration}</div>
+        <div>{formatDuration(info.cp, { withMs: true})} / {formatDuration(info.duration ?? 0, { withMs: true })}</div>
         { cover
           ? <Image
             src={cover}
