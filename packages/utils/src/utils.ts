fb2dfe1acfae82f6cb4de16989c344479d417016
Wittawas Nakkasem <vittee@hotmail.com>
2025-03-12 03:51:42 +0700

    refactor(utils): add formatDuration function

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -217,3 +217,26 @@ export async function groupByAsync<T, K extends string>(items: T[], getKey: (o: 
     return o;
   }, {} as Record<K, T[]>)
 }
+
+export function formatDuration(seconds: number, options?: { withMs?: boolean }) {
+  if (seconds <= 0) {
+    return;
+  }
+
+  const parts = [
+    [1/(60 * 60), 24, true], // hours
+    [1/60, 60], // minutes
+    [1, 60], // seconds
+    [100, 100]
+  ] as Array<[multiplier: number, modulus: number, optional: boolean | undefined]>;
+
+  const [h, m, s, ms] = parts
+    .map(([mul, mod, optional]) => {
+      const v = Math.trunc(seconds * mul) % mod;
+      return (v !== 0 || !optional) ? `${v}`.padStart(2, '0') : undefined;
+    })
+
+  const result = [h, m, s].filter(v => v !== undefined).join(':');
+
+  return options?.withMs ? `${result}.${ms}` : result;
+}
