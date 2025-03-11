d2d63e88989da3093ff856b1dc3db8c37108b1be
Wittawas Nakkasem <vittee@hotmail.com>
2025-03-12 03:48:33 +0700

    refactor(core): add `fetchAudioProps` function

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,4 +1,8 @@
 import { distance } from "fastest-levenshtein";
+import { castArray } from "lodash";
+import { AudioProperties } from "@seamless-medley/medley";
+import { BoomBoxTrack } from "./playout";
+import { MetadataHelper } from "./metadata";
 
 export function stringSimilarity(a: string, b: string): number {
   if (!a || !b) {
@@ -37,4 +41,18 @@ export function formatSongBanner(options: SongBannerFormatOptions): string | und
   }
 
   return info.length ? info.join(separators?.title ?? ' - ') : undefined;
+}
+
+export function fetchAudioProps(track: BoomBoxTrack): Promise<AudioProperties> | undefined {
+  const { extra } = track;
+
+  if (!extra) {
+    return;
+  }
+
+  if (extra.maybeAudioProperties === undefined) {
+    extra.maybeAudioProperties = MetadataHelper.audioProperties(track.path);
+  }
+
+  return extra.maybeAudioProperties;
 }
