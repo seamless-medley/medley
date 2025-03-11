6b293eb5c851d052d579081aec6c8275d7c9aaed
Wittawas Nakkasem <vittee@hotmail.com>
2025-03-12 05:08:11 +0700

    refactor(radio): the `createModel` function now create a new class having the name specified

--- packages/radio/src/db/models/base.ts
+++ packages/radio/src/db/models/base.ts
@@ -6,34 +6,36 @@ type WithActualObject<T> = {
   [$ActualObject]: T;
 }
 
-export function createModel<R extends WithId>(name?: string) {
-  const Model = class {
-    [$ActualObject]!: R;
+export function createModel<R extends WithId>(name: string) {
+  const Model = ({
+    [name]: class {
+      [$ActualObject]!: R;
 
-    constructor(r: R) {
-      this[$ActualObject] = r;
+      constructor(r: R) {
+        this[$ActualObject] = r;
 
-      return new Proxy(this, {
-        get(target, p) {
-          return (p in target) ? (target as any)[p] : r[p as keyof R];
-        },
+        return new Proxy(this, {
+          get(target, p) {
+            return (p in target) ? (target as any)[p] : r[p as keyof R];
+          },
 
-        set(target, p, value) {
-          if (p in target) {
-            (target as any)[p] = value;
-            return true;
+          set(target, p, value) {
+            if (p in target) {
+              (target as any)[p] = value;
+              return true;
+            }
+
+            if (p in r) {
+              r[p as keyof R]= value;
+              return true;
+            }
+
+            return false;
           }
-
-          if (p in r) {
-            r[p as keyof R]= value;
-            return true;
-          }
-
-          return false;
-        }
-      });
+        });
+      }
     }
-  }
+  })[name];
 
   return Model as unknown as new (r: R) => R & WithActualObject<R>;
 }
