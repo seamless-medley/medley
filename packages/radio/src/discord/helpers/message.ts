29c4cacdafe910992727d8149745fd6f166bdc2e
Wittawas Nakkasem <vittee@hotmail.com>
2025-03-16 06:28:10 +0700

    refactor(radio): the `createCoverImageAttachment` function now accept helperDomain parameter

--- packages/radio/src/discord/helpers/message.ts
+++ packages/radio/src/discord/helpers/message.ts
@@ -7,8 +7,8 @@ export type CoverImageAttachment = {
   url: string;
 }
 
-export async function createCoverImageAttachment({ extra, path }: StationTrack): Promise<CoverImageAttachment | undefined> {
-  const coverAndLyrics = await (extra?.maybeCoverAndLyrics ?? MetadataHelper.coverAndLyrics(path));
+export async function createCoverImageAttachment({ extra, path }: StationTrack, helperDomain = 'track-message'): Promise<CoverImageAttachment | undefined> {
+  const coverAndLyrics = await (extra?.maybeCoverAndLyrics ?? MetadataHelper.for(helperDomain, helper => helper.coverAndLyrics(path)));
 
   if (!coverAndLyrics) {
     return;
