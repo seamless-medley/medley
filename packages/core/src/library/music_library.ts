200e556bd15cc1325bdc0a9bb9b29075930b9a50
Wittawas Nakkasem <vittee@hotmail.com>
2025-03-16 05:44:51 +0700

    perf(core): music library now use its own MetadataHelper instance

--- packages/core/src/library/music_library.ts
+++ packages/core/src/library/music_library.ts
@@ -247,7 +247,8 @@ export class MusicLibrary<O> extends BaseLibrary<MusicTrackCollection<O>, MusicL
     let modified = false;
 
     if (force || !track.extra?.tags) {
-      const { metadata, timestamp, modified: metadataUpdated } = await MetadataHelper.fetchMetadata(track, this.musicDb, force);
+      const { metadata, timestamp, modified: metadataUpdated } = await MetadataHelper.for(`library-${this.id}`, helper => helper.fetchMetadata(track, this.musicDb, force));
+
       track.musicId = metadata.isrc,
       track.extra = {
         ...track.extra,
