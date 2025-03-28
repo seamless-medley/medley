4cce9e49ed4679c4afe18264004c1eb6bd2a2a82
Wittawas Nakkasem <vittee@hotmail.com>
2025-03-28 09:20:13 +0700

    feat(ui): add links to stations in the home page

--- packages/radio/src/ui/pages/home.tsx
+++ packages/radio/src/ui/pages/home.tsx
@@ -1,16 +1,59 @@
-import { createRoute } from "@tanstack/react-router";
+import { createRoute, Link } from "@tanstack/react-router";
 import { rootRoute } from './rootRoute';
+import { tree as playRoute } from './play/route';
+import { useSurrogate } from "../hooks/surrogate";
+import { StubGlobal } from "../stubs/core/global";
+import { useEffect, useState } from "react";
+import { stationRoute } from "./dj/route";
 
 const route = createRoute({
   getParentRoute: () => rootRoute,
   path: '/',
-  component: () => (
-    <div>
-      <div>
+  component: () => {
+    const { surrogate: $global } = useSurrogate(StubGlobal, 'global', '$');
 
+    const [stations, setStations] = useState<string[]>([]);
+
+    useEffect(() => {
+      if (!$global ) {
+        return;
+      }
+
+      $global.getStations().then(setStations);
+    }, [$global]);
+
+    return (
+      <div>
+        <h2>Play</h2>
+        <div>
+          {stations.map((statioId) => (
+            <div key={statioId}>
+              <Link
+                to={playRoute.id}
+                params={{ station: statioId }}
+              >
+                { statioId }
+              </Link>
+            </div>
+          ))}
+        </div>
+
+        <h2>DJ</h2>
+        <div>
+          {stations.map((statioId) => (
+            <div key={statioId}>
+              <Link
+                to={stationRoute.id}
+                params={{ station: statioId }}
+              >
+                { statioId }
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 });
 
 export const tree = route;
