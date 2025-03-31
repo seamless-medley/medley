87431063193c4e2dc778164bcd3c4980ba0e09d1
Wiktor Małyska <wiktormalyska03@gmail.com>
2025-03-31 14:52:45 +0200

    feat(core): make instanceName optional in configuration

--- packages/radio/src/server/medley-server.ts
+++ packages/radio/src/server/medley-server.ts
@@ -23,7 +23,7 @@ import { MedleyAutomaton } from "../discord/automaton";
 import { AutomatonConfig } from "../config/automaton";
 import { StreamingConfig } from "../config/streaming";
 import { StreamingAdapter } from "../streaming/types";
-import { ShoutAdapter } from "../streaming";
+import { ShoutAdapter } from "../streaming/shout/adapter";
 import { IcyAdapter } from "../streaming";
 import { UserModel } from '../db/models/user';
 import { retryable } from "@seamless-medley/utils";
@@ -39,7 +39,7 @@ export type MedleyServerOptions = {
 }
 
 export class MedleyServer extends SocketServerController<RemoteTypes> {
-  #instanceName: string = "Medley";
+  #instanceName: string | undefined = "Medley";
 
   #musicDb!: MusicDb;
 
