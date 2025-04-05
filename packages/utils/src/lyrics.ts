563bc6b7974b473444d6e6b75e6a709dcf76d3ae
Wittawas Nakkasem <vittee@hotmail.com>
2025-04-06 01:51:04 +0700

    feat(utils): the `parseLyrics` now supports enhanced lyrics

--- packages/utils/src/lyrics.ts
+++ packages/utils/src/lyrics.ts
@@ -1,33 +1,16 @@
 import { chain, clamp, findIndex, flatMap, max, reject, some } from "lodash";
 
-export type LyricLine = {
-  time: number;
-  text: string;
-  far?: boolean;
-}
-
-export type Timeline = LyricLine[];
-
-type Line = {
+type ParsedLine = {
   infos: [string, string][];
   times: number[];
   text: string;
-}
-
-export type Lyrics = {
-  infos: Record<string, string[]>;
-  timeline: Timeline;
 }
 
 const tagExpr = /^\s*(\[([^\]]*)\])/;
 const infoExpr = /([^\d:]+):\s*(.*)\s*/;
 const timeExpr = /(\d+):(\d+\.\d+)/;
 
-type MaybeLine = Line | undefined;
-
-const isLyricLine = (line: any): line is LyricLine => line && 'time' in line;
-
-function parseLine(line: string): MaybeLine {
+function parseLine(line: string): ParsedLine | undefined {
   const tags = [];
   let m: RegExpMatchArray | null;
 
@@ -71,9 +54,40 @@ function parseLine(line: string): MaybeLine {
   }
 }
 
+export type LyricLine<T = string> = {
+  time: number;
+  far?: boolean;
+  line: T;
+}
+
+export type Timeline<T = string> = LyricLine<T>[];
+
+export type SyncLyrics = {
+  type: 'sync';
+  infos: Record<string, string[]>;
+  timeline: Timeline;
+}
+
+export type EnhancedLineElement = {
+  time: number;
+  token: string;
+};
+
+export type EnhancedLine = Array<EnhancedLineElement>;
+
+export type EnhancedLyrics = {
+  type: 'enhanced';
+  infos: Record<string, string[]>;
+  timeline: Timeline<EnhancedLine>;
+}
+
+export type Lyrics = SyncLyrics | EnhancedLyrics;
+
 export type ParseLyricOptions = {
   bpm?: number;
 }
+
+const isLyricLine = (line: any): line is LyricLine => line && 'time' in line;
 
 export function parseLyrics(s: string, { bpm = 90 }: ParseLyricOptions = {}): Lyrics {
   const lines = s.replace(/\r\n/g, "\n").split(/\n/).map(parseLine);
@@ -86,13 +100,13 @@ export function parseLyrics(s: string, { bpm = 90 }: ParseLyricOptions = {}): Ly
 
   const offset = max((infos.offset || []).map(Number)) || 0;
 
-  const timeline = flatMap<MaybeLine, LyricLine | undefined>(lines, line => {
+  const timeline = flatMap<ParsedLine | undefined, LyricLine | undefined>(lines, line => {
     if (line === undefined) {
       return;
     }
 
     const { text, times } = line;
-    return times.map(time => ({ time: time - offset, text }))
+    return times.map(time => ({ time: time - offset, line: text }))
   });
 
   if (some(timeline, isLyricLine)) {
@@ -124,12 +138,12 @@ export function parseLyrics(s: string, { bpm = 90 }: ParseLyricOptions = {}): Ly
         const newTimes = Array(gaps).fill(0).map((t, i) => current.time + (i * duration));
 
         let fillers = chain(newTimes)
-          .map(time => ({ time: clamp(time, idlingTime, next.time), text: '' }))
+          .map(time => ({ time: clamp(time, idlingTime, next.time), line: '' }))
           .sortBy('time')
           .value()
 
         const dups = chain(fillers)
-          .filter(f => f.text === '')
+          .filter(f => f.line === '')
           .groupBy('time')
           .omitBy(group => group.length <= 1)
           .map<[number, number]>(group => [ group![0].time, group!.length - 1 ])
@@ -150,7 +164,7 @@ export function parseLyrics(s: string, { bpm = 90 }: ParseLyricOptions = {}): Ly
   const finalTimeline = chain(timeline)
     .filter(isLyricLine)
     .sortBy('time')
-    .dropRightWhile(line => line.time >= 59940000 || /^\*{3}.*\*{3}$/.test(line.text))
+    .dropRightWhile(({ time, line }) => time >= 59940000 || /^\*{3}.*\*{3}$/.test(line))
     .value()
 
   if (bpm > 0) {
@@ -162,7 +176,7 @@ export function parseLyrics(s: string, { bpm = 90 }: ParseLyricOptions = {}): Ly
     while (i < finalTimeline.length) {
       // be careful with this, lodash's findIndex() should always be used here
       // since Array.findIndex does not accept the `fromIndex` parameter hence the loop will not advance to the next line
-      const next = findIndex(finalTimeline, ({ text }) => text.trim().length > 0, i + 1);
+      const next = findIndex(finalTimeline, ({ line }) => line.trim().length > 0, i + 1);
 
       if (next > lastNext) {
         lastNext = next;
@@ -186,18 +200,66 @@ export function parseLyrics(s: string, { bpm = 90 }: ParseLyricOptions = {}): Ly
     }
   }
 
+  if (finalTimeline.find(({ line }) => /<\d+:\d+\.\d+?>/g.test(line))) {
+    let lastLinetokens: EnhancedLine | undefined = undefined;
+
+    const timeline = finalTimeline.map<LyricLine<EnhancedLine>>(({ time, far, line }) => {
+      const matches = Array.from(line.matchAll(/<(\d+):(\d+\.\d+)>/g));
+
+      const tokens: EnhancedLine = [];
+
+      for (let i = 0; i < matches.length; i++) {
+        const [curr, next] = [matches[i], matches[i+1]];
+        const [ts, mm, ss] = curr;
+        //
+        const token = line.substring(curr.index + ts.length, next?.index);
+        const time = (+mm*60 + +ss) * 1000;
+
+        tokens.push({ token, time: time - offset });
+      }
+
+      // The right most info of the each line should end with an empty string
+      // but if that's not the case, add the stop mark using current line's start time
+      if (lastLinetokens?.at(-1)?.token) {
+        lastLinetokens.push({
+          token: '',
+          time
+        })
+      }
+
+      lastLinetokens = tokens;
+
+      return {
+        time,
+        far,
+        line: tokens
+      }
+    });
+
+    return {
+      type: 'enhanced',
+      infos,
+      timeline
+    }
+  }
+
+
   return {
+    type: 'sync',
     infos,
     timeline: finalTimeline
   };
 }
 
 export const lyricsToText = (lyrics: Lyrics, removeEmptyLine: boolean = true) => {
-  const texts = lyrics.timeline.map(({ text }) => text);
+  const texts = lyrics.type === 'enhanced'
+    ? lyrics.timeline.map(({ line }) => line.join(''))
+    : lyrics.timeline.map(({ line }) => line);
+
   return removeEmptyLine ? texts.filter(text => !!text) : texts;
 }
 
-export function findLyricLine(timeline: Timeline, posMs: number, from: number = 0) {
+export function findLyricLine(timeline: Timeline<any>, posMs: number, from: number = 0) {
   for (let i = Math.max(0, from); i < timeline.length; i++) {
     const { time: l } = timeline[i];
     const { time: r } = timeline[i + 1] ?? { time: 1e7 };
