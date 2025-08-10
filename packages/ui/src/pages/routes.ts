8c93792bfcefaf1b789a0eb6b01c59eaf8d6876d
Wittawas Nakkasem <vittee@hotmail.com>
2025-08-11 01:05:52 +0700

    chore(ui): clean up

--- packages/ui/src/pages/routes.ts
+++ packages/ui/src/pages/routes.ts
@@ -1,21 +1,4 @@
-// import { rootRoute } from './rootRoute';
-// import { layoutRoute } from './layoutRoute';
-// import { indexRoute } from './home';
-// import { djRoute } from './dj/route';
-// import { playRoute } from './play/route';
-// import { createRouter } from '@tanstack/react-router';
-
 import { index, layout, rootRoute, route } from "@tanstack/virtual-file-routes";
-
-// const routeTree = rootRoute.addChildren([
-//   layoutRoute.addChildren([
-//     indexRoute
-//   ]),
-//   djRoute,
-//   playRoute
-// ]);
-
-// export const router = createRouter({ routeTree });
 
 export const routes = rootRoute('root.tsx', [
   index('home/HomePage/route.tsx'),
