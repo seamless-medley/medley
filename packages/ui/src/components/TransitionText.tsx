1e2568893683738ae78cfd6e2395ca17b10d48b9
Wittawas Nakkasem <vittee@hotmail.com>
2025-08-15 21:38:40 +0700

    refactor(ui): re-implement TransitionText

--- packages/ui/src/components/TransitionText.tsx
+++ packages/ui/src/components/TransitionText.tsx
@@ -1,25 +1,111 @@
-import React, { PropsWithChildren } from "react";
+import React, { CSSProperties, PropsWithChildren, useEffect, useRef, useState } from "react";
 import { Text, TextProps } from "@mantine/core";
-import TextTransition, { TextTransitionProps } from 'react-text-transition';
+import { useSpring, useTransition, animated, SpringConfig } from '@react-spring/web';
+import { AutoScroller } from "@ui/components/AutoScoller";
 
-type TransitionTextProps = PropsWithChildren<TextProps & TextTransitionProps> & {
-  component?: any;
-};
+type TransitionTextProps = PropsWithChildren<Omit<TextProps, 'style'>> & {
+  className?: string;
+  delay?: number;
+  direction?: 'up' | 'down';
+  inline?: boolean;
+  nowrap?: boolean;
+  springConfig?: SpringConfig;
+  translateValue?: string;
+  style?: CSSProperties;
+  autoscroll?: boolean;
+}
 
 export const TransitionText: React.FC<TransitionTextProps> = React.memo((props) => {
   const {
     children,
-    direction, inline, delay, springConfig, style, translateValue,
+    direction,
+    translateValue: tv = '100%',
+    inline,
+    nowrap,
+    style,
+    delay,
+    springConfig,
+    autoscroll,
+    className,
     ...textProps
   } = props;
 
-  const transitionProps = { direction, inline, delay, springConfig, style, translateValue };
+  const display =  inline ? 'inline-flex' : 'flex';
+  const initialRun = useRef(true);
+  const fromTransform = direction === 'down' ? `-${tv}` : tv;
+  const leaveTransform = direction === 'down' ? tv : `-${tv}`;
+
+  const transitions = useTransition([children], {
+    enter: { opacity: 1, transform: 'translateY(0%)' },
+    from: { opacity: 0, transform: `translateY(${fromTransform})` },
+    leave: {
+      opacity: 0,
+      transform: `translateY(${leaveTransform})`,
+      position: 'absolute',
+      width: 'calc(100% - 2em)',
+      overflow: 'hidden'
+    },
+    config: springConfig,
+    immediate: initialRun.current,
+    delay: !initialRun.current ? delay : undefined
+  });
+
+  const [width, setWidth] = useState<number>(0);
+  const currentRef = useRef<HTMLDivElement>(null);
+  const heightRef = useRef<number | string>('auto');
+
+  useEffect(() => {
+    initialRun.current = false;
+    const element = currentRef.current;
+
+    // If element doesn't exist, then do nothing
+    if (!element) return;
+
+    const { width, height } = element.getBoundingClientRect();
+
+    setWidth(width);
+    heightRef.current = height;
+  }, [children, setWidth, currentRef]);
+
+  const widthTransition = useSpring({
+    to: { width },
+    config: springConfig,
+    immediate: initialRun.current,
+    delay: !initialRun.current ? delay : undefined,
+
+  });
+
+  const transitionRenderer: Parameters<typeof transitions>[0] = (styles, item, state) => {
+    const element = (
+      <animated.div
+        style={{ ...styles }}
+        ref={item === children ? currentRef : undefined}
+      >
+        <Text {...textProps}>
+          {item}
+        </Text>
+      </animated.div>
+    );
+
+    const shouldScroll = state.phase !== 'leave' && autoscroll;
+
+    return shouldScroll
+      ? <AutoScroller>{element}</AutoScroller>
+      : element
+  }
 
   return (
-    <TextTransition {...transitionProps}>
-      <Text {...textProps}>
-        {children}
-      </Text>
-    </TextTransition>
+    <animated.div
+      className={`text-transition ${className}`}
+      style={{
+        ...(inline && !nowrap && !initialRun.current ? widthTransition : undefined),
+        ...style,
+        whiteSpace: (inline || nowrap) ? 'nowrap' : 'normal',
+        height: heightRef.current,
+        display
+      }}
+    >
+      {transitions(transitionRenderer)}
+    </animated.div>
   )
 });
