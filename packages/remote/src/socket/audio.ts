7e281e0afd0a4f0385f6773ea8d87899c2ef5cbf
Wittawas Nakkasem <vittee@hotmail.com>
2025-08-31 00:24:00 +0700

    refactor(remote): remove `AudioSocketCommandMap` type

--- packages/remote/src/socket/audio.ts
+++ packages/remote/src/socket/audio.ts
@@ -4,12 +4,6 @@ export type AudioSocketCommand_Detune = 2;
 
 export type AudioSocketCommand = AudioSocketCommand_Identify | AudioSocketCommand_Tune | AudioSocketCommand_Detune;
 
-export type AudioSocketCommandMap = {
-  [0 /* AudioSocketCommand_Identify */]: (socketId: string) => void;
-  [1 /* AudioSocketCommand_Tune */]: (stationId: string) => void;
-  [2 /* AudioSocketCommand_Detune */]: () => void;
-}
-
 export type AudioSocketReply_Latency = 0x00;
 export type AudioSocketReply_Opus = 0xCC;
 
