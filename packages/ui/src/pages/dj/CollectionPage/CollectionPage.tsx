12b8271416e5be0c935440fbee857948771dfbe7
Wittawas Nakkasem <vittee@hotmail.com>
2025-09-08 22:43:16 +0700

    refactor(ui): clean up

--- packages/ui/src/pages/dj/CollectionPage/CollectionPage.tsx
+++ packages/ui/src/pages/dj/CollectionPage/CollectionPage.tsx
@@ -2,19 +2,19 @@ import { Box, Group } from "@mantine/core";
 import { styled } from "@linaria/react";
 import { useParams } from "@tanstack/react-router";
 import { useCollection } from "@ui/hooks/useCollection";
-
 import { useRemotableProp } from "@ui/hooks/remotable";
 import { CollectionTracks } from "../components/CollectionTracks";
 
 
-const PageContainer = styled(Box)`
+const Container = styled(Box)`
   display: flex;
   flex-direction: column;
   height: calc(100vh - 60px - 200px); // Viewport minus AppShell header minus TopBar
 `;
 
-const CollectionHeader = styled(Group)`
+const Header = styled(Group)`
   flex-shrink: 0; // Prevent shrinking
+  justify-content: space-between;
 `;
 
 export const CollectionPage = () => {
@@ -23,16 +23,15 @@ export const CollectionPage = () => {
   const description = useRemotableProp(collection, 'description') ?? collectionId;
   const length = useRemotableProp(collection, 'length', 0);
 
-  // Render
   return (
-    <PageContainer>
-      <CollectionHeader mx='md' justify="space-between">
+    <Container>
+      <Header mx='md'>
         <h2>{description} - {length} tracks</h2>
         <span>TODO: Buttons here</span>
-      </CollectionHeader>
+      </Header>
 
       <CollectionTracks collection={collection} />
-    </PageContainer>
+    </Container>
   )
 }
 
