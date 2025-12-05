be4310b2a3524aaae9e4648d3a7b2356c94b8a00
Wittawas Nakkasem <vittee@hotmail.com>
2025-12-06 00:27:32 +0700

    refactor(radio): remove pactching codes for the workerpool worker

--- packages/radio/src/core/worker_pool_adapter.ts
+++ packages/radio/src/core/worker_pool_adapter.ts
@@ -11,74 +11,14 @@ export abstract class WorkerPoolAdapter<Methods extends { [name: string]: any }>
   constructor(script: string, options: WorkerPoolOptions) {
     const { workerThreadOpts = {}, ...restOptions } = options;
 
-    if (process.env.NODE_ENV === 'development') {
-      if (!(workerThreadOpts.execArgv?.length ?? 0)) {
-        workerThreadOpts.execArgv = (workerThreadOpts.execArgv ?? []).concat(['--require', '@swc-node/register'])
-      }
-    }
-
-    this.pool = this.#patchPool(workerpool.pool(script, {
+    this.pool = workerpool.pool(script, {
       ...restOptions,
       workerThreadOpts
-    }));
+    });
   }
 
   protected exec<M extends keyof Methods>(m: M, ...args: Parameters<Methods[M]>) {
     return this.pool.exec<Methods[M]>(m as string, args);
-  }
-
-  #patchSend(handler: any) {
-    const worker = handler.worker;
-
-    if (!worker.patched) {
-      worker.send = (message: any) => {
-        try {
-          worker.postMessage(message);
-        }
-        catch (e: any) {
-          this.logger.error(e, 'Error posting');
-        }
-      }
-
-      worker.patched = true;
-    }
-  }
-
-  #patchPool(pool: WorkerPool) {
-    const p = pool as any;
-    p._next = () => {
-      if (p.tasks.length > 0) {
-        const worker = p._getWorker();
-
-        if (worker) {
-          const task = p.tasks.shift();
-
-          if (!task.resolver.promise.pending) {
-            p._next();
-            return;
-          }
-
-          this.#patchSend(worker);
-
-          const promise = worker.exec(task.method, task.params, task.resolver, task.options)
-            .then(p._boundNext)
-            .catch(() => {
-              if (worker.terminated) {
-                return p._removeWorker(worker);
-              }
-            })
-            .then(() => {
-              p._next();
-            });
-
-          if (typeof task.timeout === 'number') {
-            promise.timeout(task.timeout);
-          }
-        }
-      }
-    }
-
-    return pool;
   }
 
   protected preSpawn(n: number = workerpool.cpus) {
