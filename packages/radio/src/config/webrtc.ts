f9a6e83397a5a43f225ac9795c8456959fff3f2b
Wittawas Nakkasem <vittee@hotmail.com>
2025-12-25 18:55:41 +0700

    refactor(radio): webrtc `announcedIp` property is now required when the `ip` is set to `0.0.0.0` or `::`

--- packages/radio/src/config/webrtc.ts
+++ packages/radio/src/config/webrtc.ts
@@ -5,6 +5,13 @@ export const ListenInfo = z.object({
   ip: z.ipv4(),
   port: z.number().min(1).max(65535).optional(),
   announcedIp: z.ipv4().optional()
+}).superRefine(({ announcedIp, ip }, ctx) => {
+  if (!announcedIp && (ip === '0.0.0.0') || (ip === '::')) {
+    ctx.addIssue({
+      code: 'custom',
+      message: '`announcedIp` is required when `ip` is set to `0.0.0.0` or `::`'
+    })
+  }
 });
 
 export type ListenInfo = z.infer<typeof ListenInfo>;
