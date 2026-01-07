85aef52c7908f6ec0416df75aaad8f1cd9cfa52f
Wittawas Nakkasem <vittee@hotmail.com>
2026-01-07 23:11:38 +0700

    refactor(ui): add station validation and not found error handling to play route

--- packages/ui/src/pages/play/PlayPage/route.tsx
+++ packages/ui/src/pages/play/PlayPage/route.tsx
@@ -1,7 +1,19 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, notFound } from "@tanstack/react-router";
 
 import { PlayPage as component } from './PlayPage';
+import { client } from "@ui/init";
+import { Alert, Container, Title } from "@mantine/core";
 
-export const Route = createFileRoute('/_normal-layout/play/$station')({ component });
+export const Route = createFileRoute('/_normal-layout/play/$station')({
+  component,
+  loader: ({ params }) => client.remoteGet('station', params.station, 0, 'id').catch(notFound),
+  notFoundComponent: () => (
+    <Container>
+      <Alert variant="filled" color="red.7" radius="lg">
+        <Title order={2}>No such station</Title>
+      </Alert>
+    </Container>
+  )
+});
 
 export const PlayRoute = Route;
