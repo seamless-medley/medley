302cc7ea58a92a732b72ad4e445bcd1ed829bec5
Wittawas Nakkasem <vittee@hotmail.com>
2026-02-01 19:56:06 +0700

    refactor(ui): update theme colors and anchor styling

--- packages/ui/src/theme/theme.ts
+++ packages/ui/src/theme/theme.ts
@@ -7,15 +7,29 @@ import 'overlayscrollbars/overlayscrollbars.css';
 import { fontFamily } from './fonts';
 import classes from './global.module.css';
 
-const primaryColor = 'pink';
+const primaryColor = 'blue';
 
 export const themeOverride = createTheme({
   fontFamily,
   primaryColor,
+  colors: {
+    dark: [
+      'hsl(205, 10%, 88.8%)',
+      'hsl(205, 10%, 70.15%)',
+      'hsl(205, 10%, 48.98%)',
+      'hsl(205, 10%, 39.18%)',
+      'hsl(205, 10%, 23.87%)',
+      'hsl(205, 10%, 21.13%)',
+      'hsl(205, 10%, 16.04%)',
+      'hsl(205, 10%, 12.12%)',
+      'hsl(205, 10%, 10.16%)',
+      'hsl(205, 10%, 4.88%)'
+    ]
+  },
   defaultGradient: {
     deg: 90,
     from: primaryColor,
-    to: 'violet'
+    to: 'grape'
   }
 });
 
