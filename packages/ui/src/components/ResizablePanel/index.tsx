1a47d61343befc6e90e1c8153a031b8231616f05
Wittawas Nakkasem <vittee@hotmail.com>
2026-02-15 18:54:46 +0700

    refactor(ui): improve ResizablePanel typing

--- packages/ui/src/components/ResizablePanel/index.tsx
+++ packages/ui/src/components/ResizablePanel/index.tsx
@@ -2,7 +2,7 @@ import { Panel as PanelInternal } from './Panel/Panel';
 import { PanelGroup } from './PanelGroup/PanelGroup';
 import { Resizer } from './Resizer/Resizer';
 
-const p = PanelInternal as any;
+const p = PanelInternal as typeof ResizablePanel;
 
 p.Group = PanelGroup;
 p.Resizer = Resizer;
