8fbb09b66d0573f0e64f46a2c898fede3e02bd18
Wittawas Nakkasem <vittee@hotmail.com>
2026-02-19 23:46:00 +0700

    refactor(radio): extract BasedExposedCollectionView to separate file

--- packages/radio/src/server/expose/core/collection.ts
+++ packages/radio/src/server/expose/core/collection.ts
@@ -4,15 +4,12 @@ import type {
   MusicTrack,
   MusicTrackCollection,
   MusicTrackCollectionEvents,
-  Station,
-  Track as CoreTrack,
-  TrackCollectionView,
+  Station
 } from "../../../core";
 
 import { MixinEventEmitterOf } from "../../socket";
 
 import type {
-  BaseCollectionView,
   Collection,
   CollectionView,
   Exposable,
@@ -21,6 +18,7 @@ import type {
   Track as RemoteTrack
 } from "@seamless-medley/remote";
 import { toRemoteTrackRecord, toRemoteTrack } from "./track";
+import { BasedExposedCollectionView } from "./collection_view";
 
 export class ExposedCollection extends MixinEventEmitterOf<Collection>() implements Exposable<Collection> {
   $Exposing: MusicTrackCollection<Station>;
@@ -116,93 +114,6 @@ export class ExposedCollection extends MixinEventEmitterOf<Collection>() impleme
   createView(numItems: number, topIndex?: number) {
     // This casting is required because the type presented to the client must be `Remotable` instead of `Exposable`
     return new ExposedCollectionView(this.#collection.createView(numItems, topIndex)) as unknown as Remotable<CollectionView>;
-  }
-}
-
-export abstract class BasedExposedCollectionView<T extends CoreTrack<any>> extends MixinEventEmitterOf<BaseCollectionView<any>>() {
-  $Exposing: TrackCollectionView<T>;
-  $Kind = 'collection_view';
-  notify!: Notify<CollectionView>;
-
-  constructor(view: TrackCollectionView<T>) {
-    super();
-
-    this.$Exposing = view;
-
-    view.on('viewChange',  this.#onViewChange);
-  }
-
-  dispose(): void {
-    this.#view.off('viewChange',  this.#onViewChange);
-    this.#view.dispose();
-  }
-
-  #onViewChange = () => {
-    this.emit('viewChange');
-  }
-
-  get #view() {
-    return this.$Exposing;
-  }
-
-  get length() {
-    return this.#view.length;
-  }
-
-  set length(val) {
-    this.#view.length = val;
-  }
-
-  get topIndex() {
-    return this.#view.topIndex;
-  }
-
-  set topIndex(val) {
-    this.#view.topIndex = val;
-  }
-
-  get bottomIndex() {
-    return this.#view.bottomIndex;
-  }
-
-  get ranges() {
-    return this.#view.ranges;
-  }
-
-  updateView(topIndex: number, length: number): void {
-    this.#view.updateView(topIndex, length);
-  }
-
-  absolute(localIndex: number): number {
-    return this.#view.absolute(localIndex);
-  }
-
-  isIndexInView(absoluteIndex: number): boolean {
-    return this.#view.isIndexInView(absoluteIndex);
-  }
-
-  protected abstract toRemoteTrack(track: T): Promise<RemoteTrack>;
-
-  protected abstract toRemoteMetadataOnlyTrack(track: T): Array<any>;
-
-  async at(index: number) {
-    const track = this.#view.at(index);
-    return track ? this.toRemoteTrack(track) : undefined;
-  }
-
-  async items() {
-    return Promise.all(this.#view.items().map(item => this.toRemoteTrack(item)));
-  }
-
-  itemsWithIndexes(): Array<[index: number, track: any]> {
-    return this.#view
-      .itemsWithIndexes()
-      .map(([index, track]) =>
-        [
-          index,
-          this.toRemoteMetadataOnlyTrack(track)
-        ] as [index: number, track: any]
-      );
   }
 }
 
---
+++ packages/radio/src/server/expose/core/collection_view.ts
@@ -0,0 +1,99 @@
+import type {
+  BaseCollectionView,
+  Notify,
+  Track as RemoteTrack
+} from "@seamless-medley/remote";
+
+import type {
+  Track as CoreTrack,
+  TrackCollectionView,
+} from "../../../core";
+
+import { MixinEventEmitterOf } from "../../socket";
+
+export abstract class BasedExposedCollectionView<T extends CoreTrack<any>> extends MixinEventEmitterOf<BaseCollectionView<any>>() {
+  $Exposing: TrackCollectionView<T>;
+  $Kind = 'collection_view';
+  notify!: Notify<BaseCollectionView<any>>;
+
+  constructor(view: TrackCollectionView<T>) {
+    super();
+
+    this.$Exposing = view;
+
+    view.on('viewChange',  this.#onViewChange);
+  }
+
+  dispose(): void {
+    this.#view.off('viewChange',  this.#onViewChange);
+    this.#view.dispose();
+  }
+
+  #onViewChange = () => {
+    this.emit('viewChange');
+  }
+
+  get #view() {
+    return this.$Exposing;
+  }
+
+  get length() {
+    return this.#view.length;
+  }
+
+  set length(val) {
+    this.#view.length = val;
+  }
+
+  get topIndex() {
+    return this.#view.topIndex;
+  }
+
+  set topIndex(val) {
+    this.#view.topIndex = val;
+  }
+
+  get bottomIndex() {
+    return this.#view.bottomIndex;
+  }
+
+  get ranges() {
+    return this.#view.ranges;
+  }
+
+  updateView(topIndex: number, length: number): void {
+    this.#view.updateView(topIndex, length);
+  }
+
+  absolute(localIndex: number): number {
+    return this.#view.absolute(localIndex);
+  }
+
+  isIndexInView(absoluteIndex: number): boolean {
+    return this.#view.isIndexInView(absoluteIndex);
+  }
+
+  protected abstract toRemoteTrack(track: T): Promise<RemoteTrack>;
+
+  protected abstract toRemoteMetadataOnlyTrack(track: T): Array<any>;
+
+  async at(index: number) {
+    const track = this.#view.at(index);
+    return track ? this.toRemoteTrack(track) : undefined;
+  }
+
+  async items() {
+    return Promise.all(this.#view.items().map(item => this.toRemoteTrack(item)));
+  }
+
+  itemsWithIndexes(): Array<[index: number, track: any]> {
+    return this.#view
+      .itemsWithIndexes()
+      .map(([index, track]) =>
+        [
+          index,
+          this.toRemoteMetadataOnlyTrack(track)
+        ] as [index: number, track: any]
+      );
+  }
+}
