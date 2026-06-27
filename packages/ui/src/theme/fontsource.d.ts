f2cf2adcf2d85004c0ddc6e7cc89f534f2d5d69f
Wittawas Nakkasem <vittee@hotmail.com>
2026-06-28 00:00:30 +0700

    fix(ui): add fontsource types shim

---
+++ packages/ui/src/theme/fontsource.d.ts
@@ -0,0 +1,4 @@
+declare module '@fontsource-variable/exo-2';
+declare module '@fontsource-variable/anuphan';
+declare module '@fontsource-variable/murecho';
+declare module '@fontsource/gowun-dodum';
